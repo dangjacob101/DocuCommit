d837b14dbbc29ec03c09998f884941ef03083b0c
samsam324 <sammyadham@gmail.com>
2026-05-08 16:55:51 -0700

    Add listCommits API helper

--- client/src/api.js
+++ client/src/api.js
@@ -59,3 +59,7 @@ export function diffBranch(branchId, ignoreWhitespace = false) {
 export function mergeBranch(branchId) {
   return send('POST', `/api/branches/${branchId}/merge`)
 }
+
+export function listCommits(branchId) {
+  return send('GET', `/branches/${branchId}/commits`)
+}
